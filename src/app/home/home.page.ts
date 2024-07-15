88576756c12de58a9a542c6a04544d20fdbbd3a3
sarvosmi <vaishali.nimbolkar@hotmail.com>
2024-07-15 15:33:26 +0530

    first commit

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import {MyCustomPlugin} from 'my-custom-plugin';
 
 @Component({
   selector: 'app-home',
@@ -8,5 +9,33 @@ import { Component } from '@angular/core';
 export class HomePage {
 
   constructor() {}
+
+  async testPluginMethod(msg: string) {
+    
+    await MyCustomPlugin.testPluginMethod({msg:msg})
+    .then((res:any)=>{alert("Return value is " +JSON.stringify(res.value))})
+    .catch((err:any)=>{alert(JSON.stringify(err))})
+  }
+
+  async pluginPermissionMethod()
+  {
+    alert("pluginPermissionMethod")
+    await MyCustomPlugin.pluginPermissionMethod()
+    .then((res:any)=>{alert(JSON.stringify(res))})
+    .catch((err:any)=>{alert(JSON.stringify(err))})
+  }
+  async openWebsite()
+  {
+    await MyCustomPlugin.openWebsite({siteURL:"https://capacitorjs.com/docs/plugins"})
+    .then((res:any)=>{alert(JSON.stringify(res))})
+    .catch((err:any)=>{alert(JSON.stringify(err))})
+  }
+
+  async calenderTest()
+  {
+    await MyCustomPlugin.calenderTest()
+    .then((res:any)=>{alert(JSON.stringify(res))})
+    .catch((err:any)=>{alert(JSON.stringify(err))})
+  }
 
 }
